dfad86ce55c5e54a6d0e222fb14e3b1ac455a101
Reveritus <153584119+Reveritus@users.noreply.github.com>
2024-06-30 14:26:08 +0300

    Create BuildConfig.java

---
+++ COM/PUSHKIN/hotdoged/fido/BuildConfig.java
@@ -0,0 +1,10 @@
+package com.pushkin.hotdoged.fido;
+
+public final class BuildConfig {
+    public static final String APPLICATION_ID = "com.pushkin.hotdoged.fido";
+    public static final String BUILD_TYPE = "release";
+    public static final boolean DEBUG = false;
+    public static final String FLAVOR = "";
+    public static final int VERSION_CODE = 26;
+    public static final String VERSION_NAME = "2.14.5";
+}
