1a1e643afe3060b2fe077799bb3fb276240eba4b
Reveritus <153584119+Reveritus@users.noreply.github.com>
2024-06-30 13:46:29 +0300

    Create AreaListParser.java

---
+++ COM/PUSHKIN/Area/AreaListParser.java
@@ -0,0 +1,7 @@
+package com.pushkin.area;
+
+import com.pushkin.hotdoged.export.HotdogedException;
+
+public interface AreaListParser {
+    void parse(AreaList areaList) throws HotdogedException;
+}
