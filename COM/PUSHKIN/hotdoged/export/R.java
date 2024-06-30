d591ab967712924bcb5d03cf6649387b2e669d87
Reveritus <153584119+Reveritus@users.noreply.github.com>
2024-06-30 14:10:59 +0300

    Create R.java

---
+++ COM/PUSHKIN/hotdoged/export/R.java
@@ -0,0 +1,21 @@
+package com.pushkin.hotdoged.export;
+
+public final class R {
+
+    public static final class drawable {
+        public static final int ic_launcher = 0x7f020054;
+    }
+
+    public static final class layout {
+        public static final int main = 0x7f030023;
+    }
+
+    public static final class string {
+        public static final int app_name = 0x7f060027;
+    }
+
+    public static final class style {
+        public static final int AppBaseTheme = 0x7f08000b;
+        public static final int AppTheme = 0x7f0800a4;
+    }
+}
